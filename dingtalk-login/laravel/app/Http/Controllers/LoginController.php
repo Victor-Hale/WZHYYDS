cd20025981a910286a98a32a52e1ad3fd98997ca
wzhyyds123 <1179378262@qq.com>
2021-11-01 19:31:19 +0800

    钉钉第三方登录

---
+++ dingtalk-login/laravel/app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function tests()
+    {
+
+        $time = time() . '000';#毫秒时间戳
+        $s = hash_hmac('sha256', $time, '73mDFc0NZR7CkZwiy9WZdEkobo6YW_R2_JU3CEeoRaKKQPdmE3ETkoi83xLCFb_X', true);
+        $signature = base64_encode($s);
+        $urlencode_signature = urlencode($signature);#签名
+
+
+        $remote_server = 'https://oapi.dingtalk.com/sns/getuserinfo_bycode?accessKey=dingoa8zw8llesz1rnrjq5&timestamp=' . $time . '&signature=' . $urlencode_signature;#地址组装
+        $post_string = json_encode(['tmp_auth_code' => $_GET['code']]);#扫码获得的临时code
+        $json = $this->PostCurlRequest($remote_server, $post_string);
+        dd(($json));#获得用户相关信息
+
+
+    }
+
+    /**
+     * post 请求
+     * @param $remote_server
+     * @param $post_string
+     * @return bool|string
+     */
+    protected function PostCurlRequest($remote_server, $post_string)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $remote_server);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json;charset=utf-8'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
+}
---
+++ dingtalk-login/laravel/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+/**
+ * @author wzh
+ */
+Route::prefix('dingding')->group(function (){
+    Route::get('login','LoginController@tests');
+});
+  Route::view('login0','login');
